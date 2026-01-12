069e7da93a4cb6f20a57ba41e7eda35fa392bf8c
18621851698 <robert.cha@polariscode.org>
2026-01-12 16:49:48 +0800

    1.0.18

--- lib/mind_map_node.dart
+++ lib/mind_map_node.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_mind_map/adapter/i_node_adapter.dart';
 import 'package:flutter_mind_map/i_mind_map_node.dart';
